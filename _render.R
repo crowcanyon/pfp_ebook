30ac00973ae97ff538256d3f728b043e8844881a
bocinsky <bocinsky@gmail.com>
2017-11-28 18:01:38 -0700

    fixed sharing buttons

--- _render.R
+++ _render.R
@@ -10,15 +10,13 @@ owd = setwd(dirname(src))
 
 # provide default formats if necessary
 if (length(formats) == 0) formats = c(
-  'bookdown::pdf_book', 'bookdown::epub_book', 'bookdown::gitbook'
+  'bookdown::pdf_book', 'bookdown::gitbook'
 )
 # render the book to all formats unless they are specified via command-line args
 for (fmt in formats) {
   cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
   res = bookdown:::Rscript(c('-e', shQuote(cmd)))
   if (res != 0) stop('Failed to compile the book to ', fmt)
-  if (!travis && fmt == 'bookdown::epub_book')
-    bookdown::calibre('docs/pfp_ebook.epub', 'mobi')
 }
 
 r = '<body onload="window.location = \'https://www.crowcanyon.org/institute\'+location.pathname">'
